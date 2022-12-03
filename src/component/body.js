6aff077f25518331f143b31ef6289c27c5cae06f
Shafin chowdhury <ShafinChowdhury2001@gmail.com>
2022-12-03 22:01:05 +0600

    new message

---
+++ src/component/body.js
@@ -0,0 +1,18 @@
+// import React,{component} from "react";
+
+// class Body extends component{
+//     render(){
+//         return(
+//             <div>
+
+          
+//           <h4>Shafin</h4>
+            
+        
+//             </div>
+           
+//         )
+//     }
+// }
+
+// export default Body;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './component/header';
+ import Body from './component/body';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+<Header/>
+<Body/>
+
   </React.StrictMode>
 );
 
